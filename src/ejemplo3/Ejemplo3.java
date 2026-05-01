30222ce79d6b00c86992418b81cfb95e559bc4c0
Haydee Delgado <dhelgado723@gmail.com>
2026-05-01 16:25:14 -0500

    Ejemplo 3 - Clase ejemplo3 actualizada

--- src/ejemplo3/Ejemplo3.java
+++ src/ejemplo3/Ejemplo3.java
@@ -4,6 +4,8 @@
  */
 package ejemplo3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author USUARIO
@@ -15,6 +17,44 @@ public class Ejemplo3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Scanner sc = new Scanner(System.in);
+        AsistenteControlador controla = new AsistenteControlador ();
+        String rpta = "si";
+        
+        while(rpta.equals("si"))
+        {
+            Asistente a = new Asistente();
+            while(a.getTipo_documento()== null)
+            {
+                System.out.println("Ingrese el tipo de documento: ");
+                a.setTipo_documento(sc.nextLine());
+            }
+            while(a.getNumero_documento()==null)
+            {
+                System.out.println("Ingrese el numero de documento: ");
+                a.setNumero_documento(sc.nextLine());
+            }
+            System.out.println("Ingrese el Apellido Paterno: ");
+            a.setApellido_paterno(sc.nextLine());
+            System.out.println("Ingrese el Apellido Materno: ");
+            a.setApellido_materno(sc.nextLine());
+            System.out.println("Ingrese el Nombre: ");
+            a.setNombre(sc.nextLine());
+            System.out.println("Ingrese el Celular: ");
+            a.setCelular(sc.nextLine());
+            System.out.println("Ingrese el Correo Electronico: ");
+            a.setCorreo(sc.nextLine());
+            while(a.getEdad()==0)
+            {
+                System.out.println("Ingrese la edad: ");
+                a.setEdad(sc.nextInt());
+            }
+            sc.nextLine();
+            controla.agregar_asistente(a);
+            System.out.println("Desea agregar a otro asitente: si/no:  ");
+            rpta=sc.nextLine();
+        }
+        controla.lista_asistentes();
     }
-    
 }
